92db9c31cdde115baa6d52bd3ea8cc10f20152a0
Thomas Schmidt <thomas5862@gmail.com>
2018-05-02 17:17:01 -0700

    Broken

--- app/src/main/java/group10/tcss450/uw/edu/chatterbox/LoginFragment.java
+++ app/src/main/java/group10/tcss450/uw/edu/chatterbox/LoginFragment.java
@@ -5,11 +5,17 @@ import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
+import android.text.Editable;
+import android.text.SpannableStringBuilder;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import group10.tcss450.uw.edu.chatterbox.model.Credentials;
 
 
 /**
@@ -37,20 +43,52 @@ public class LoginFragment extends Fragment {
             Log.e("Error", "login title isn't working");
         }
 
+        // variables
         Button loginButton = v.findViewById(R.id.buttonLoginLogin);
-        loginButton.setOnClickListener(view -> mListener.onLoginAction("test", "test"));
-
         Button registerButton = v.findViewById((R.id.buttonLoginRegister));
+        EditText usernameTextBox = (EditText) v.findViewById(R.id.editTextLoginEmail);
+        EditText passwordTextBox = (EditText) v.findViewById(R.id.editTextLoginPassword);
+
+        loginButton.setOnClickListener(view -> {
+            boolean flag = true;
+
+            if(usernameTextBox.getText().toString().length() < 1) {
+                usernameTextBox.setError("Must have a username!");
+                flag = false;
+            }
+            if(passwordTextBox.getText().length() < 1) {
+                passwordTextBox.setError("Must have a password!");
+                flag = false;
+            }
+            if(flag) {
+                Editable password = new SpannableStringBuilder(passwordTextBox.getText().toString());
+                String username = usernameTextBox.getText().toString();
+                Credentials.Builder mCredentials = new Credentials.Builder(username, password);
+                Credentials mc = mCredentials.build();
+                mListener.onLoginAttempt(mc);
+            }
+        });
+
         registerButton.setOnClickListener(view -> mListener.onLoginRegisterAction());
 
         return v;
     }
 
-
-
-    public interface OnFragmentInteractionListener {
-        void onLoginAction(String username, String password);
-        void onLoginRegisterAction();
+    /**
+     * Allows an external source to set an error message on this fragment. This may
+     * be needed if an Activity includes processing that could cause login to fail.
+     * @param err the error message to display.
+     */
+    /**
+     * Allows an external source to set an error message on this fragment. This may
+     * be needed if an Activity includes processing that could cause login to fail.
+     * @param err the error message to display.
+     */
+    public void setError(String err) {
+//        Log in unsuccessful for reason: err. Try again.
+//        you may want to add error stuffs for the user here.
+        ((TextView) getView().findViewById(R.id.logUsernameEditText))
+                .setError("Login Unsuccessful");
     }
 
     @Override
@@ -68,5 +106,11 @@ public class LoginFragment extends Fragment {
     public void onDetach() {
         super.onDetach();
         mListener = null;
+    }
+
+    public interface OnFragmentInteractionListener {
+        void onLoginAttempt(Credentials credentials);
+        void onLoginAction();
+        void onLoginRegisterAction();
     }
 }
--- app/src/main/java/group10/tcss450/uw/edu/chatterbox/MainActivity.java
+++ app/src/main/java/group10/tcss450/uw/edu/chatterbox/MainActivity.java
@@ -1,5 +1,6 @@
 package group10.tcss450.uw.edu.chatterbox;
 
+import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.net.Uri;
@@ -9,6 +10,7 @@ import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.CheckBox;
 import android.widget.EditText;
 
 import org.json.JSONException;
@@ -46,7 +48,9 @@ public class MainActivity extends AppCompatActivity
         }
 
         setContentView(R.layout.activity_main);
-        loadFragment(new LoginFragment());
+        getSupportFragmentManager().beginTransaction()
+                .add(R.id.fragmentContainer, new LoginFragment(), getString(R.string.keys_fragment_login))
+                .commit();
     }
 
     private void loadFragment(Fragment frag) {
@@ -70,6 +74,55 @@ public class MainActivity extends AppCompatActivity
      * a JSON formatted String. Parse it for success or failure.
      * @param result the JSON formatted String response from the web service
      */
+    private void handleLoginOnPost(String result) {
+        try {
+            JSONObject resultsJSON = new JSONObject(result);
+            boolean success = resultsJSON.getBoolean("success");
+            if (success) {
+                //Login was successful. Switch to the loadSuccessFragment.
+                checkStayLoggedIn();
+                onLoginAction();
+            } else {
+                //Login was unsuccessful. Don’t switch fragments and inform the user
+                LoginFragment frag =
+                        (LoginFragment) getSupportFragmentManager()
+                                .findFragmentByTag(
+                                        getString(R.string.keys_fragment_login));
+                frag.setError("Log in unsuccessful");
+            }
+        } catch (JSONException e) {
+            //It appears that the web service didn’t return a JSON formatted String
+            //or it didn’t have what we expected in it.
+            Log.e("JSON_PARSE_ERROR", result
+                    + System.lineSeparator()
+                    + e.getMessage());
+        }
+    }
+
+    private void checkStayLoggedIn() {
+        if (((CheckBox) findViewById(R.id.checkBoxLoginStayLogIn)).isChecked()) {
+            SharedPreferences prefs =
+                    getSharedPreferences(
+                            getString(R.string.keys_shared_prefs),
+                            Context.MODE_PRIVATE);
+            //save the username for later usage
+            prefs.edit().putString(
+                    getString(R.string.keys_prefs_username),
+                    mCredentials.getUsername())
+                    .apply();
+            //save the users “want” to stay logged in
+            prefs.edit().putBoolean(
+                    getString(R.string.keys_prefs_stay_logged_in),
+                    true)
+                    .apply();
+        }
+    }
+
+    /**
+     * Handle onPostExecute of the AsynceTask. The result from our webservice is
+     * a JSON formatted String. Parse it for success or failure.
+     * @param result the JSON formatted String response from the web service
+     */
     private void handleRegOnPost(String result) {
         try {
             JSONObject resultsJSON = new JSONObject(result);
@@ -86,9 +139,9 @@ public class MainActivity extends AppCompatActivity
                         .findFragmentByTag(
                                 getString(R.string.keys_fragment_register));*/
 
-                RegisterFragment frag = (RegisterFragment) getSupportFragmentManager()
-                        .findFragmentById(R.id.fragmentContainer);
-                frag.setError("Register unsuccessful");
+//                RegisterFragment frag = (RegisterFragment) getSupportFragmentManager()
+//                        .findFragmentById(R.id.fragmentContainer);
+//                frag.setError("Register unsuccessful");
             }
 
         } catch (JSONException e) {
@@ -102,7 +155,7 @@ public class MainActivity extends AppCompatActivity
     }
 
     @Override
-    public void onLoginAction(String username, String password) {
+    public void onLoginAction() {
         //Load home activity
         startActivity(new Intent(MainActivity.this, HomeActivity.class));
     }
@@ -129,6 +182,27 @@ public class MainActivity extends AppCompatActivity
         //Load registration fragment
         loadFragment(regFrag);
 
+    }
+
+    @Override
+    public void onLoginAttempt(Credentials credentials) {
+        //build the web service URL
+        Uri uri = new Uri.Builder()
+                .scheme("https")
+                .appendPath(getString(R.string.ep_base_url))
+                .appendPath(getString(R.string.ep_register))
+                .build();
+        //build the JSONObject
+        JSONObject msg = credentials.asJSONObject();
+        mCredentials = credentials;
+        //instantiate and execute the AsyncTask.
+        //Feel free to add a handler for onPreExecution so that a progress bar
+        //is displayed or maybe disable buttons. You would need a method in
+        //LoginFragment to perform this.
+        new SendPostAsyncTask.Builder(uri.toString(), msg)
+                .onPostExecute(this::handleLoginOnPost)
+                .onCancelled(this::handleErrorsInTask)
+                .build().execute();
     }
 
     @Override
