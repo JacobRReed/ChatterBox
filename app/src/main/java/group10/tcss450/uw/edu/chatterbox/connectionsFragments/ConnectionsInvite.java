d1c4589e214731573d7a913855da0bd7a48a316d
Jacob Reed <jacob.russell.reed@gmail.com>
2018-05-06 16:35:43 -0700

    Added all framework for connections

--- app/src/main/java/group10/tcss450/uw/edu/chatterbox/connectionsFragments/ConnectionsInvite.java
+++ app/src/main/java/group10/tcss450/uw/edu/chatterbox/connectionsFragments/ConnectionsInvite.java
@@ -1,11 +1,21 @@
 package group10.tcss450.uw.edu.chatterbox.connectionsFragments;
 
 
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.ContactsContract;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import group10.tcss450.uw.edu.chatterbox.R;
 
@@ -13,7 +23,8 @@ import group10.tcss450.uw.edu.chatterbox.R;
  * A simple {@link Fragment} subclass.
  */
 public class ConnectionsInvite extends Fragment {
-
+    private Button mContactsButton;
+    private static final int CONTACT_PICKER_RESULT = 1001;
 
     public ConnectionsInvite() {
         // Required empty public constructor
@@ -24,7 +35,80 @@ public class ConnectionsInvite extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_connections_invite, container, false);
+        View v = inflater.inflate(R.layout.fragment_connections_invite, container, false);
+        mContactsButton = v.findViewById(R.id.inviteContactButton);
+        mContactsButton.setOnClickListener(this::onContactClick);
+        Button inviteButton = v.findViewById(R.id.inviteButton);
+        inviteButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                //Send the invite via email using web call
+            }
+        });
+
+        return v;
+    }
+
+    private void onContactClick(View v) {
+        Intent contactPickerIntent = new Intent(Intent.ACTION_PICK,
+                ContactsContract.Contacts.CONTENT_URI);
+        startActivityForResult(contactPickerIntent, CONTACT_PICKER_RESULT);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (resultCode == Activity.RESULT_OK) {
+            switch (requestCode) {
+                case CONTACT_PICKER_RESULT:
+                    Cursor cursor = null;
+                    String email = "";
+                    try {
+                        Uri result = data.getData();
+                        Log.v("", "Got a contact result: "
+                                + result.toString());
+
+                        // get the contact id from the Uri
+                        String id = result.getLastPathSegment();
+
+                        // query for everything email
+                        cursor = getContext().getContentResolver().query(ContactsContract.CommonDataKinds.Email.CONTENT_URI,
+                                null, ContactsContract.CommonDataKinds.Email.CONTACT_ID + "=?", new String[] { id },
+                                null);
+
+                        int emailIdx = cursor.getColumnIndex(ContactsContract.CommonDataKinds.Email.DATA);
+
+                        // let's just get the first email
+                        if (cursor.moveToFirst()) {
+                            email = cursor.getString(emailIdx);
+                            Log.v("", "Got email: " + email);
+                        } else {
+                            Log.w("", "No results");
+                        }
+                    } catch (Exception e) {
+                        Log.e("", "Failed to get email data", e);
+                    } finally {
+                        if (cursor != null) {
+                            cursor.close();
+                        }
+                        EditText emailEntry = getActivity().findViewById(R.id.inviteEmail);
+                        emailEntry.setText(email);
+                        if (email.length() == 0) {
+                            Toast.makeText(getContext(), "No email found for contact.",
+                                    Toast.LENGTH_LONG).show();
+                        }
+
+                    }
+
+                    break;
+            }
+
+        } else {
+            // gracefully handle failure
+            Log.w("", "Warning: activity result not ok");
+        }
+
+
     }
 
 }
--- app/src/main/java/group10/tcss450/uw/edu/chatterbox/connectionsFragments/ConnectionsRequests.java
+++ app/src/main/java/group10/tcss450/uw/edu/chatterbox/connectionsFragments/ConnectionsRequests.java
@@ -3,16 +3,26 @@ package group10.tcss450.uw.edu.chatterbox.connectionsFragments;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import java.util.ArrayList;
+
 import group10.tcss450.uw.edu.chatterbox.R;
+import group10.tcss450.uw.edu.chatterbox.utils.Contact;
+import group10.tcss450.uw.edu.chatterbox.utils.ContactsAdapterRequests;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class ConnectionsRequests extends Fragment {
+    private RecyclerView mRecyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
+    public ArrayList<Contact> mContacts;
 
 
     public ConnectionsRequests() {
@@ -23,8 +33,39 @@ public class ConnectionsRequests extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_connections_requests, container, false);
+        View v = inflater.inflate(R.layout.fragment_connections_requests, container, false);
+
+        //Recycler view for result
+        mRecyclerView = v.findViewById(R.id.requestsRecycler);
+        mRecyclerView.setHasFixedSize(true);
+        mLayoutManager = new LinearLayoutManager(getContext());
+        //Generate contacts
+        mContacts = new ArrayList<>();
+        createContacts();
+        mAdapter = new ContactsAdapterRequests(mContacts);
+        mRecyclerView.setAdapter(mAdapter);
+        mRecyclerView.setLayoutManager(mLayoutManager);
+
+        return v;
     }
 
+
+    private void createContacts() {
+        mContacts.add(new Contact("test1"));
+        mContacts.add(new Contact("test2"));
+        mContacts.add(new Contact("test3"));
+        mContacts.add(new Contact("test4"));
+        mContacts.add(new Contact("test5"));
+        mContacts.add(new Contact("test6"));
+        mContacts.add(new Contact("test7"));
+        mContacts.add(new Contact("test8"));
+        mContacts.add(new Contact("test9"));
+        mContacts.add(new Contact("test10"));
+        mContacts.add(new Contact("test11"));
+        mContacts.add(new Contact("test12"));
+        mContacts.add(new Contact("test13"));
+        mContacts.add(new Contact("test14"));
+        mContacts.add(new Contact("test15"));
+
+    }
 }
