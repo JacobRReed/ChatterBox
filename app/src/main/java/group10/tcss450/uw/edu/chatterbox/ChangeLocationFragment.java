bfad63170d5ae7227ccb178182bfc0a6916b9580
Jacob Reed <jacob.russell.reed@gmail.com>
2018-05-16 12:23:22 -0700

    a

--- app/src/main/java/group10/tcss450/uw/edu/chatterbox/ChangeLocationFragment.java
+++ app/src/main/java/group10/tcss450/uw/edu/chatterbox/ChangeLocationFragment.java
@@ -1,22 +1,57 @@
 package group10.tcss450.uw.edu.chatterbox;
 
 
+import android.Manifest;
 import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.MapsInitializer;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class ChangeLocationFragment extends Fragment {
+public class ChangeLocationFragment extends Fragment{
     private OnFragmentInteractionListener mListener;
+    private SupportMapFragment mSupportMapFragment;
+    private View mView;
+    private GoogleMap mGoogleMap;
+    private GoogleApiClient mGoogleApiClient;
+    private LocationRequest mLocationRequest;
+    private Location mCurrentLocation;
+    private static final String TAG = "MyLocationsFragment";
 
     public ChangeLocationFragment() {
         // Required empty public constructor
@@ -27,7 +62,7 @@ public class ChangeLocationFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View v = inflater.inflate(R.layout.fragment_change_location, container, false);
+        mView = inflater.inflate(R.layout.fragment_change_location, container, false);
 
 
         try {
@@ -36,12 +71,38 @@ public class ChangeLocationFragment extends Fragment {
             Log.e("Error", "title isn't working");
         }
 
-        Button submitChangeButton = v.findViewById(R.id.buttonChangeLocationSetChanges);
+        Button submitChangeButton = mView.findViewById(R.id.buttonChangeLocationSetChanges);
         submitChangeButton.setOnClickListener(view -> mListener.onChangeLocationSubmitAction());
 
-        return v;
-    }
 
+        //Map stuff
+        mSupportMapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.mapView);
+        if (mSupportMapFragment == null) {
+            FragmentManager fragmentManager = getFragmentManager();
+            FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+            mSupportMapFragment = SupportMapFragment.newInstance();
+            fragmentTransaction.replace(R.id.mapView, mSupportMapFragment).commit();
+        }
+        if (mSupportMapFragment != null)
+        {
+            mSupportMapFragment.getMapAsync(new OnMapReadyCallback() {
+                @Override public void onMapReady(GoogleMap googleMap) {
+                    if (googleMap != null) {
+                        mGoogleMap = googleMap;
+                        mGoogleMap.getUiSettings().setAllGesturesEnabled(true);
+                        LatLng marker_latlng = new LatLng(47.2529, -122.4443);
+
+                        CameraPosition cameraPosition = new CameraPosition.Builder().target(marker_latlng).zoom(15.0f).build();
+                        CameraUpdate cameraUpdate = CameraUpdateFactory.newCameraPosition(cameraPosition);
+                        mGoogleMap.moveCamera(cameraUpdate);
+
+                    }
+
+                }
+            });}
+
+        return mView;
+    }
 
     public interface OnFragmentInteractionListener {
         void onChangeLocationSubmitAction();
