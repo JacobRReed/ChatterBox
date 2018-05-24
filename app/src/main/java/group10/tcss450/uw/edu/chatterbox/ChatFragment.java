031fe670eb7d64943d99acf28fe9df1dd1bc9ec1
Jacob Reed <jacob.russell.reed@gmail.com>
2018-05-23 21:06:48 -0700

    a

--- app/src/main/java/group10/tcss450/uw/edu/chatterbox/ChatFragment.java
+++ app/src/main/java/group10/tcss450/uw/edu/chatterbox/ChatFragment.java
@@ -5,7 +5,6 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
-import android.text.method.ScrollingMovementMethod;
 import android.util.Log;
 import android.util.TypedValue;
 import android.view.LayoutInflater;
